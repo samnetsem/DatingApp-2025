2b4c76554ecc549c65856c8cde75aed8d834331a
Samy <samnetsem@gmail.com>
2026-01-08 00:51:59 +0300

    End of Section 5

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import {FormsModule} from "@angular/forms"
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css',
+})
+export class Nav {
+  protected creds: any={}
+  protected accountService=inject(AccountService);
+  login(){
+    this.accountService.login(this.creds).subscribe({
+      next: result=>console.log(result),
+      error: error=> alert(error)
+    })
+  }
+  logout()
+  {
+    this.accountService.logout();
+  }
+}
